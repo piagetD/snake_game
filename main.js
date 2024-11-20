a88b9da48a54054548569c97257f787de844195c
yuzd_legion <1330735019@qq.com>
2024-11-20 23:32:46 +0800

    todo

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const mapCount = 30; // 地图格子的个数
 const direction = ["up", "down", "left", "right"]
 const BORDER = width * mapCount;
 let head;
-let x = 0;
-let y = 0;
+let snake = [];
+let x = 100;
+let y = 100;
 let isRun = false; // 按下按钮之后开始
+let preDirection = "right"; // 默认自动的方向
 
 function checkBorder() {
     if (x < 0 || x > BORDER || y < 0 || y > BORDER) {
@@ -19,12 +21,31 @@ function checkBorder() {
     }
 }
 
+function addBody(x, y) {
+    let body = document.createElement("div");
+    body.className = "block";
+    body.style.setProperty("top", y + "px")
+    body.style.setProperty("left", x + "px")
+    document.getElementById("map").appendChild(body);
+    snake.push(body);
+    console.log(snake);
+}
+
 function resetGame() {
     head = document.getElementById('snake');
-    x = 0;
-    y = 0;
+    x = 100;
+    y = 100;
     head.style.setProperty("top", y + "px")
     head.style.setProperty("left", x + "px")
+}
+// 应该是无限循环的函数，除非游戏中止
+function autoAction() {
+
+}
+
+// 根据方向前进，遍历整个body数组，依次更新
+function snakeForward() {
+
 }
 
 function moveAction(el, direction) {
@@ -75,6 +96,7 @@ function init() {
         head = document.getElementById('snake');
         map = document.getElementById('map');
         let mapStyle = map.style;
+        addBody(x - width, y); // 第一个子body
         mapStyle.setProperty("width", width * mapCount + "px")
         mapStyle.setProperty("height", width * mapCount + "px")
     };
